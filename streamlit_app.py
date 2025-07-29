39b42663f7afac372bd8d652f62ebe075cde676e
Justin A. Kader <jukader@gmail.com>
2025-07-29 14:53:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,38 +93,38 @@ except RegisterError as e:
     st.error(e)
 
 # Creating a forgot password widget
-try:
-    (username_of_forgotten_password,
-     email_of_forgotten_password,
-     new_random_password) = authenticator.forgot_password()
-    if username_of_forgotten_password:
-        st.success(f"New password **'{new_random_password}'** to be sent to user securely")
-        config['credentials']['usernames'][username_of_forgotten_password]['pp'] = new_random_password
-        # Random password to be transferred to the user securely
-    elif not username_of_forgotten_password:
-        st.error('Username not found')
-except ForgotError as e:
-    st.error(e)
+#try:
+#    (username_of_forgotten_password,
+#     email_of_forgotten_password,
+#     new_random_password) = authenticator.forgot_password()
+#    if username_of_forgotten_password:
+#        st.success(f"New password **'{new_random_password}'** to be sent to user securely")
+#        config['credentials']['usernames'][username_of_forgotten_password]['pp'] = new_random_password
+#        # Random password to be transferred to the user securely
+#    elif not username_of_forgotten_password:
+#        st.error('Username not found')
+#except ForgotError as e:
+#    st.error(e)
 
 # Creating a forgot username widget
-try:
-    (username_of_forgotten_username,
-     email_of_forgotten_username) = authenticator.forgot_username()
-    if username_of_forgotten_username:
-        st.success(f"Username **'{username_of_forgotten_username}'** to be sent to user securely")
-        # Username to be transferred to the user securely
-    elif not username_of_forgotten_username:
-        st.error('Email not found')
-except ForgotError as e:
-    st.error(e)
+#try:
+#    (username_of_forgotten_username,
+#     email_of_forgotten_username) = authenticator.forgot_username()
+#    if username_of_forgotten_username:
+#        st.success(f"Username **'{username_of_forgotten_username}'** to be sent to user securely")
+#        # Username to be transferred to the user securely
+#    elif not username_of_forgotten_username:
+#        st.error('Email not found')
+#except ForgotError as e:
+#    st.error(e)
 
 # Creating an update user details widget
-if st.session_state["authentication_status"]:
-    try:
-        if authenticator.update_user_details(st.session_state["username"]):
-            st.success('Entries updated successfully')
-    except UpdateError as e:
-        st.error(e)
+#if st.session_state["authentication_status"]:
+#    try:
+#        if authenticator.update_user_details(st.session_state["username"]):
+#            st.success('Entries updated successfully')
+#    except UpdateError as e:
+#        st.error(e)
 
 # Saving config file
 with open('config.yaml', 'w', encoding='utf-8') as file:
