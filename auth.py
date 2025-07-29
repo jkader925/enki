8f846194e67472da690e800a0becc8ee4ab5ebde
Justin A. Kader <jukader@gmail.com>
2025-07-29 13:06:54 -0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,35 +1,24 @@
 # auth.py
-import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 from pathlib import Path
-from settings import USERS_FILE, COOKIE_NAME, COOKIE_KEY
 
-def load_users():
-    if USERS_FILE.exists():
-        with open(USERS_FILE) as f:
-            return yaml.load(f, Loader=SafeLoader)
-    else:
-        return {"usernames": {}}
+USERS_FILE = Path("users.yaml")
 
-def save_users(users):
-    with open(USERS_FILE, "w") as f:
-        yaml.dump(users, f)
+def load_authenticator():
+    with open(USERS_FILE, "r") as file:
+        config = yaml.load(file, Loader=SafeLoader)
 
-def get_authenticator():
-    users = load_users()
-    return stauth.Authenticate(
-        users,
-        COOKIE_NAME,
-        COOKIE_KEY,
-        cookie_expiry_days=30
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days'],
+        config['preauthorized']
     )
+    return authenticator, config
 
-def login():
-    authenticator = get_authenticator()
-    name, auth_status, username = authenticator.login("Login", location="main")
-    return authenticator, name, auth_status, username
-
-def logout(authenticator):
-    authenticator.logout("Logout", location="sidebar")
+def save_user_config(config):
+    with open(USERS_FILE, "w") as file:
+        yaml.dump(config, file)
